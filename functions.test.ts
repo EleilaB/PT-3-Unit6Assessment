e731949eacb73ae27890575edbd7da44559a3692
Eleila Bradford <eleila.bradford@gmail.com>
2022-08-28 03:23:39 -0600

    initial commit, unfinished

---
+++ functions.test.ts
@@ -0,0 +1,9 @@
+const {shuffleArray} = require('./utils')
+describe('shuffleArray', () => {
+    it('returns an array', () => {
+        expect(shuffleArray).toBeTruthy()
+    })
+    // it('returns an array that is the same length as the argument sent in', () => {
+    //     expect(null).toBeTruthy()
+    // })
+})
